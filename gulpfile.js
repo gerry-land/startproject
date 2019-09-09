7dd7fdbb065784885a1f7ebc9f3c5cde81c3d54a
gerry <gerry@zeoalliance.com>
2019-09-09 17:11:11 +0300

    update services section

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,55 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass'); 
+var sass = require('gulp-sass');
 var browser = require('browser-sync');
 var postCss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var htmlInclude = require('gulp-lb-include');
 var clean = require('gulp-clean');
+var svgLoader = require('gulp-embed-svg');
 
 // handle html
 gulp.task('html', function () {
   return gulp.src('app/index.html')
     .pipe(htmlInclude())
+    .pipe(gulp.dest('app/temp/'))
+    .pipe(browser.reload({ stream: true }))
+});
+
+// handle svg
+gulp.task('svg-loader', async function () {
+  gulp.src('./app/temp/index.html')
+    .pipe(svgLoader({
+      root: './app/images',
+      selector: '.to-inline-svg'
+    }))
     .pipe(gulp.dest('public/'))
-    .pipe(browser.reload({stream: true}))
 });
 
 // handle styles
-gulp.task('styles', async function() {
+gulp.task('styles', async function () {
   return gulp.src('./app/scss/index.scss', {
-            sourcemaps: true
-          })
+    sourcemaps: true
+  })
     .pipe(sass())
     .pipe(postCss([autoprefixer()]))
     .pipe(gulp.dest('public'))
-    .pipe(browser.reload({stream: true}))
+    .pipe(browser.reload({ stream: true }))
 });
 
 // handle scripts
-gulp.task('script', async function() {
+gulp.task('script', async function () {
   return gulp.src('app/js/index.js')
     .pipe(gulp.dest('public'))
-    .pipe(browser.reload({stream: true}))
+    .pipe(browser.reload({ stream: true }))
 });
 
 // run local server for developing
-gulp.task('browser', async function() {
+gulp.task('browser', async function () {
   browser({
     server: {
       baseDir: 'public'
     },
-    notify: false 
+    notify: false
   })
 });
 
@@ -54,20 +65,20 @@ gulp.task('copyFonts', async function () {
     .pipe(gulp.dest('public/fonts'))
 });
 
-gulp.task('clean', function() {
-  return gulp.src('public', {read: false, allowEmpty: true})
-    .pipe(clean({force: true}))
+gulp.task('clean', function () {
+  return gulp.src('public', { read: false, allowEmpty: true })
+    .pipe(clean({ force: true }))
 });
 
 // enable watch to run bundle after some changes
-gulp.task('watch', function(){
+gulp.task('watch', function () {
   gulp.watch('app/scss/**/*.scss', gulp.parallel('styles'));
   gulp.watch('app/js/index.js', gulp.parallel('script'));
-  gulp.watch('app/**/*.html', gulp.parallel('html'));
+  gulp.watch(['app/index.html', 'app/htmlChunks/**/*.html'], gulp.series('html', 'svg-loader'));
   gulp.watch('app/images/**', gulp.parallel('copy'));
   gulp.watch('app/fonts/**', gulp.parallel('copyFonts'));
-
 })
 
 // init default gulp task
-gulp.task('default', gulp.parallel('styles', 'script', 'html', 'copy', 'copyFonts', 'browser', 'watch'));
+gulp.task('default', gulp.series(gulp.parallel('styles', 'script', 'html', 'copy', 'copyFonts'),
+  'svg-loader', 'browser', 'watch'));
