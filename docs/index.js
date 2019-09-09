26831c9a49a3d1f359c7f43fd0305b2db46a6759
gerry <gerry@zeoalliance.com>
2019-09-09 17:16:14 +0300

    add g.io page folder

---
+++ docs/index.js
@@ -0,0 +1,25 @@
+function initColorText() {
+  return;
+  const text = document.getElementsByClassName('js-color-text')[0];
+
+  const colors = [
+    'orange', // 0
+    'royalblue', // 1
+    'pink', // 2
+    'coral', // 3
+    'yellow' // 4
+  ]
+
+  let index = 0;
+
+  setInterval(function() {
+    text.style = 'color: ' + colors[index];
+    if (index === 4) {
+      index = 0;
+    } else {
+      index++
+    }
+  }, 2000);
+}
+
+initColorText();
